0be1b9c835a68ab7b8261387ffb46829b7c180e7
Chris Nevill <chris@leftoverbits.co.uk>
2018-09-26 17:57:35 +0100

    fixed fastboot

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,54 +2,19 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      bower: {
-        dependencies: { }
-      }
+      name: 'default'      
     },
     {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
-        }
-      }
+      name: 'ember-1.13'    
     },
     {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      }
+      name: 'ember-release'     
     },
     {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      }
+      name: 'ember-beta'      
     },
     {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      }
+      name: 'ember-canary'
     }
   ]
 }
